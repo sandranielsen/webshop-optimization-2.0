bd3574a945155b5523529f6dad3c47b372a055b9
sandranielsen <eaasmmn@students.eaaa.dk>
2023-01-09 12:05:53 +0100

    slider ux improvement

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { graphql } from "gatsby";
-
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import Layout from "../components/Layout";
 import ProductSlider from "../components/ProductSlider";
@@ -14,6 +15,14 @@ import Seo from "../components/seo";
 const IndexPage = ({ data }) => {
   const { nodes } = data.allShopifyProduct;
   const guides = data.allMongodbMagomadeBlogGuides.edges;
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
 
   return (
@@ -28,15 +37,12 @@ const IndexPage = ({ data }) => {
           ))}
         </div>
       </div>
-      <div className="slider">
-        <div className="flex relative items-center">
-        
-          <div className="slides">
-            {guides.map((guide, index) => (
+      <div>
+        <Slider {...settings}>
+          {guides.map((guide, index) => (
               <GuideCard key={index} guide={guide} />
-            ))}
-          </div>
-        </div>
+          ))}
+        </Slider>
       </div>
     </Layout>
   );};
